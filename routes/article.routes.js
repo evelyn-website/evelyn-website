a38c3f78eddf3154178bda8c261a43d914df561f
Evelyn Nelson <153469210+evelyn-nelson@users.noreply.github.com>
2024-03-17 18:47:40 -0400

    Update article.routes.js
    change auth middleware

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -1,31 +1,31 @@
 module.exports = app => {
     const articles = require("../controllers/article.controller.js");
-    const verifyToken = require('../middleware/authmiddleware.js');
+    const authMiddleware = require('../middleware/authmiddleware.js');
     var router = require("express").Router();
   
     // Create a new Article
-    router.post("/", verifyToken, articles.create);
+    router.post("/", [authMiddleware.verifyToken], articles.create);
   
     // Retrieve all articles
-    router.get("/", verifyToken, articles.findAll);
+    router.get("/", [authMiddleware.verifyToken], articles.findAll);
   
     // Retrieve a single article with id
-    router.get("/:id", verifyToken, articles.findOne);
+    router.get("/:id", [authMiddleware.verifyToken], articles.findOne);
 
     // Retrieve a single Article with userId
-    router.get("/byUserId/:userId", verifyToken, articles.findArticleByUserId);
+    router.get("/byUserId/:userId", [authMiddleware.verifyToken], articles.findArticleByUserId);
 
     // Retrieve a single Article with userId
-    router.get("/byUsername/:username", verifyToken, articles.findArticleByUsername);
+    router.get("/byUsername/:username", [authMiddleware.verifyToken], articles.findArticleByUsername);
 
     // Update a article with id
-    router.put("/:id", verifyToken, articles.update);
+    router.put("/:id", [authMiddleware.verifyToken], articles.update);
   
     // Delete a article with id
-    router.delete("/:id", verifyToken, articles.delete);
+    router.delete("/:id", [authMiddleware.verifyToken], articles.delete);
   
     // Delete all articles
-    router.delete("/", verifyToken, articles.deleteAll);
+    router.delete("/", [authMiddleware.verifyToken], articles.deleteAll);
   
-    app.use('/api/articles', verifyToken, router);
+    app.use('/api/articles', [authMiddleware.verifyToken], router);
   };
