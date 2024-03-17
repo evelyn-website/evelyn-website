011ae6a3e7d084b78ae267e296684f4f333fb455
Evelyn Nelson <153469210+evelyn-nelson@users.noreply.github.com>
2024-03-17 18:45:27 -0400

    Update userProfile.routes.js
    Change auth middleware importing

--- routes/userProfile.routes.js
+++ routes/userProfile.routes.js
@@ -1,34 +1,34 @@
 module.exports = app => {
     const userProfiles = require("../controllers/userProfile.controller.js");
-    const verifyToken = require('../middleware/authmiddleware.js');
+    const authMiddleware = require('../middleware/authmiddleware.js');
     var router = require("express").Router();
   
     // Create a new userProfile
-    router.post("/", verifyToken, userProfiles.create);
+    router.post("/", [authMiddleware.verifyToken], userProfiles.create);
   
     // Retrieve all userProfiles
-    router.get("/", verifyToken, userProfiles.findAll);
+    router.get("/", [authMiddleware.verifyToken], userProfiles.findAll);
   
     // Retrieve a single userProfile with id
-    router.get("/:id", verifyToken, userProfiles.findOne);
+    router.get("/:id", [authMiddleware.verifyToken], userProfiles.findOne);
 
     // Retrieve a single userProfile with userId
-    router.get("/byUserId/:userId", verifyToken, userProfiles.findUserProfileByUserId);
+    router.get("/byUserId/:userId", [authMiddleware.verifyToken], userProfiles.findUserProfileByUserId);
 
     // Retrieve a single userProfile with userId
-    router.get("/byUsername/:username", verifyToken, userProfiles.findUserProfileByUsername);
+    router.get("/byUsername/:username", [authMiddleware.verifyToken], userProfiles.findUserProfileByUsername);
 
     // Update a userProfile with id
-    router.put("/:id", verifyToken, userProfiles.update);
+    router.put("/:id", [authMiddleware.verifyToken], userProfiles.update);
 
     // Update a userProfile with userId
-    router.put("/byUserId/:userId", verifyToken, userProfiles.updateByUserId);
+    router.put("/byUserId/:userId", [authMiddleware.verifyToken], userProfiles.updateByUserId);
   
     // Delete a userProfile with id
-    router.delete("/:id", verifyToken, userProfiles.delete);
+    router.delete("/:id", [authMiddleware.verifyToken], userProfiles.delete);
   
     // Delete all userProfiles
-    router.delete("/", verifyToken, userProfiles.deleteAll);
+    router.delete("/", [authMiddleware.verifyToken], userProfiles.deleteAll);
   
     app.use('/api/userProfiles', router);
   };
