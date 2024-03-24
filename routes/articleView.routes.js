33472afff02b096d3b870899c0829acba65ea836
Evelyn Nelson <evelynnelson000@gmail.com>
2024-03-24 19:27:14 -0400

    cleanup and change requirements on routes

--- routes/articleView.routes.js
+++ routes/articleView.routes.js
@@ -1,6 +1,6 @@
 module.exports = app => {
     const articleViews = require("../controllers/articleView.controller.js")
-    const {verifyToken} = require('../middleware/authmiddleware.js');
+    const {verifyToken, verifyAdmin} = require('../middleware/authmiddleware.js');
     var router = require("express").Router();
 
     // Create a new articleView
@@ -10,22 +10,22 @@ module.exports = app => {
     router.get("/", verifyToken, articleViews.findAll);
 
     // Retrieve a single articleView with id
-    router.get("/:id", verifyToken, articleViews.findOne);
+    router.get("/:id", verifyAdmin, articleViews.findOne);
 
     // Retrieve a single articleView with userId
-    router.get("/byUserId/:userId", verifyToken, articleViews.findArticleViewsByUserId);
+    router.get("/byUserId/:userId", verifyAdmin, articleViews.findArticleViewsByUserId);
 
     // Retrieve a single articleView with articleId
-    router.get("/byArticleId/:articleId", verifyToken, articleViews.findArticleViewsByArticleId);
+    router.get("/byArticleId/:articleId", verifyAdmin, articleViews.findArticleViewsByArticleId);
 
     // Update a articleView with id
-    router.put("/:id", verifyToken, articleViews.update);
+    router.put("/:id", verifyAdmin, articleViews.update);
 
     // Delete a articleView with id
-    router.delete("/:id", verifyToken, articleViews.delete);
+    router.delete("/:id", verifyAdmin, articleViews.delete);
 
     // Delete all articleViews
-    router.delete("/", verifyToken, articleViews.deleteAll);
+    router.delete("/", verifyAdmin, articleViews.deleteAll);
 
     app.use('/api/articleViews', router);
 };
--- routes/userProfile.routes.js
+++ routes/userProfile.routes.js
@@ -1,34 +1,31 @@
 module.exports = app => {
     const userProfiles = require("../controllers/userProfile.controller.js");
-    const {verifyToken} = require('../middleware/authmiddleware.js');
+    const {verifyToken, verifyAdmin} = require('../middleware/authmiddleware.js');
     var router = require("express").Router();
   
     // Create a new userProfile
-    router.post("/", verifyToken, userProfiles.create);
+    router.post("/", verifyAdmin, userProfiles.create);
   
     // Retrieve all userProfiles
-    router.get("/", verifyToken, userProfiles.findAll);
+    router.get("/", verifyAdmin, userProfiles.findAll);
   
     // Retrieve a single userProfile with id
-    router.get("/:id", verifyToken, userProfiles.findOne);
+    router.get("/:id", verifyAdmin, userProfiles.findOne);
 
     // Retrieve a single userProfile with userId
     router.get("/byUserId/:userId", verifyToken, userProfiles.findUserProfileByUserId);
 
-    // Retrieve a single userProfile with userId
-    router.get("/byUsername/:username", verifyToken, userProfiles.findUserProfileByUsername);
-
     // Update a userProfile with id
-    router.put("/:id", verifyToken, userProfiles.update);
+    router.put("/:id", verifyAdmin, userProfiles.update);
 
     // Update a userProfile with userId
     router.put("/byUserId/:userId", verifyToken, userProfiles.updateByUserId);
   
     // Delete a userProfile with id
-    router.delete("/:id", verifyToken, userProfiles.delete);
+    router.delete("/:id", verifyAdmin, userProfiles.delete);
   
     // Delete all userProfiles
-    router.delete("/", verifyToken, userProfiles.deleteAll);
+    router.delete("/", verifyAdmin, userProfiles.deleteAll);
   
     app.use('/api/userProfiles', router);
   };
