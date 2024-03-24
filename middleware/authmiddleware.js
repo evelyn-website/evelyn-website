fc95a1db27d9195e617f3701c6ee7181c151db57
Evelyn Nelson <evelynnelson000@gmail.com>
2024-03-24 19:18:36 -0400

    add admin middleware

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 function verifyToken(req, res, next) {
     var token = req.header('Cookie')
     if (!token) {return res.status(401).json({ error: 'Access denied' })}
-    var token = token.split('=')[1];
+    token = token.split('=')[1];
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.token = decoded.userId;
@@ -15,4 +15,23 @@ function verifyToken(req, res, next) {
       }   
   }
 
-  module.exports = verifyToken;
+  function verifyAdmin(req, res, next) {
+    var token = req.header('Cookie')
+    if (!token) {return res.status(401).json({ error: 'Access denied' })}
+    token = token.split('=')[1];
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.token = decoded.userId;
+        if (decoded.permissions.admin) {
+            next()
+        } else {
+            return res.status(401).json({ error: 'Access denied' })
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.status(401).json({ error: 'Invalid token' });
+      }   
+  }
+
+  module.exports = {verifyToken, verifyAdmin};
